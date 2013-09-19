213caa1da0bad511e4b01f1f5f3517bc483115a9
Christian Ayala <cayala@smu.edu>
2013-09-19 17:59:01 -0500

    Initial commit with project setup

---
+++ MyApplication/src/main/java/edu/SMU/PingItApp/MainScreenActivity.java
@@ -0,0 +1,26 @@
+package edu.SMU.PingItApp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+
+public class MainScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_screen_action_bar_items, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    
+}
