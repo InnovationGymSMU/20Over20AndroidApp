cc3beb852d7a9ae39ea9589853a4d5d4176a5576
charliealbright <albrightc25@gmail.com>
2013-09-20 00:35:17 -0500

    OMG MUSIC PLAYS NOW

--- MyApplication/src/main/java/edu/SMU/PingItApp/FindTagActivity.java
+++ MyApplication/src/main/java/edu/SMU/PingItApp/FindTagActivity.java
@@ -1,15 +1,26 @@
 package edu.SMU.PingItApp;
 
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class FindTagActivity extends Activity {
 
+    boolean isPinging;
+    Button pingButton;
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_tag);
+
+        isPinging = false;
+        pingButton = (Button) findViewById(R.id.pingButton);
+        mediaPlayer = new MediaPlayer();
     }
 
 
@@ -18,6 +29,40 @@ public class FindTagActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.find_tag, menu);
         return true;
+    }
+
+    public void playMusic(View view)
+    {
+        if(isPinging == false)
+        {
+            isPinging = true;
+            pingButton.setText("Stop Pinging");
+
+            try
+            {
+                AssetFileDescriptor descriptor = getAssets().openFd("sax.mp3");
+                mediaPlayer.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(),
+                        descriptor.getLength());
+                descriptor.close();
+                mediaPlayer.prepare();
+                mediaPlayer.setLooping(true);
+                mediaPlayer.start();
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        else if(isPinging == true)
+        {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = new MediaPlayer();
+
+            isPinging = false;
+            pingButton.setText("Start Pinging");
+        }
     }
     
 }
