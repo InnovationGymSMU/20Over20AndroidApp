c7c5d268ce47a79f8bfc19eb2e3a8e652017ea05
Christian Ayala <cayala@smu.edu>
2013-09-19 20:15:20 -0500

    Progress on home screen, dynamically add new items, ready to integrate with registration page

--- MyApplication/src/main/java/edu/SMU/PingItApp/MainScreenActivity.java
+++ MyApplication/src/main/java/edu/SMU/PingItApp/MainScreenActivity.java
@@ -1,17 +1,29 @@
 package edu.SMU.PingItApp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Debug;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
 
 public class MainScreenActivity extends Activity {
+
+    private static final String tag = "MainScreenActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        initializeListViewAdapter();
     }
 
 
@@ -22,5 +34,33 @@ public class MainScreenActivity extends Activity {
         inflater.inflate(R.menu.main_screen_action_bar_items, menu);
         return super.onCreateOptionsMenu(menu);
     }
-    
+
+    public void initializeListViewAdapter() {
+        ItemListRow[] rows = new ItemListRow[] {
+            new ItemListRow("Tag 1", null),
+            new ItemListRow("Tag 2", null)
+        };
+
+        MainScreenListArrayAdapter adapter = new MainScreenListArrayAdapter(this, R.layout.item_selection_list_row, rows);
+
+        ListView listView = (ListView)findViewById(R.id.main_screen_item_list_view);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String viewText = ((ItemListRow)parent.getItemAtPosition(position)).getTagName();
+                Log.d(tag, "Picked tag " + viewText);
+            }
+        });
+    }
+
+    public void addNewItem(MenuItem item) {
+        Log.d(tag, "Ready to start device registration");
+
+        /*
+        Intent intent = new Intent(this, MainScreenActivity.class);
+        startActivity(intent);
+        */
+
+    }
 }
---
+++ MyApplication/src/main/java/edu/SMU/PingItApp/MainScreenListArrayAdapter.java
@@ -0,0 +1,38 @@
+package edu.SMU.PingItApp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Chris on 9/19/13.
+ */
+public class MainScreenListArrayAdapter extends ArrayAdapter<ItemListRow>  {
+
+    private Context context;
+    private int rowResourceId;
+    private ItemListRow[] items;
+
+    public MainScreenListArrayAdapter(Context context, int layoutResourceId, ItemListRow[] items) {
+        super(context, layoutResourceId, items);
+        this.context = context;
+        this.rowResourceId = layoutResourceId;
+        this.items = items;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        if (row == null) {
+            LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+            row = inflater.inflate(rowResourceId, parent, false);
+        }
+        ((TextView)row.findViewById(R.id.item_name)).setText(items[position].getTagName());
+        return row;
+    }
+}
